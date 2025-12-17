86950da11f7a36c8809cf913a2ca72855af9e299
fanda <201537454@qq.com>
2025-12-18 01:32:54 +0800

    记一笔页面绘制，增加相应图标

---
+++ app/src/main/java/com/fanda/homebook/components/SelectableRoundedButton.kt
@@ -0,0 +1,92 @@
+package com.fanda.homebook.components
+
+import androidx.annotation.DrawableRes
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.fanda.homebook.R
+
+@Composable
+fun SelectableRoundedButton(
+    text: String,
+    selected: Boolean,
+    onClick: () -> Unit,
+    contentPadding: PaddingValues = PaddingValues(horizontal = 8.dp, vertical = 4.dp),
+    modifier: Modifier = Modifier,
+    cornerSize: Dp = 4.dp,
+    fontSize: TextUnit = 14.sp,
+    selectedBackgroundColor: Color = Color.Black,
+    unselectedBackgroundColor: Color = colorResource(R.color.color_D7DEE9),
+    selectedContentColor: Color = Color.White,
+    unselectedContentColor: Color = Color.Black,
+    @DrawableRes imageRes: Int? = null
+) {
+    // Surface 默认会确保其 可点击区域 ≥ 48dp
+    Box(
+        modifier = modifier
+            .clip(RoundedCornerShape(cornerSize))
+            .background(if (selected) selectedBackgroundColor else unselectedBackgroundColor)
+            .clickable {
+                onClick()
+            },
+        contentAlignment = Alignment.Center,
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Center,
+            modifier = Modifier.padding(contentPadding)
+        ) {
+            Text(
+                text = text,
+                fontSize = fontSize,
+                color = if (selected) selectedContentColor else unselectedContentColor,
+                style = TextStyle.Default,  // 用默认样式，才不会有字体的间距
+                fontWeight = if (selected) FontWeight.Medium else FontWeight.Normal,
+                modifier = modifier
+            )
+            if (imageRes != null) {
+                Image(
+                    painter = painterResource(id = imageRes),
+                    contentDescription = null,
+                    modifier = Modifier.padding(start = 4.dp)
+                )
+            }
+        }
+    }
+
+    // Surface 默认会确保其 可点击区域 ≥ 48dp
+//    Surface(
+//        onClick = onClick,
+//        modifier = modifier,
+//        shape = RoundedCornerShape(cornerSize),
+//        color = if (selected) selectedBackgroundColor else unselectedBackgroundColor,
+//        contentColor = if (selected) selectedContentColor else unselectedContentColor,
+//    ) {
+//        Row(verticalAlignment = Alignment.CenterVertically, horizontalArrangement = Arrangement.Center, modifier = Modifier.padding(contentPadding)) {
+//            Text(text = text, fontSize = fontSize, style = TextStyle.Default, fontWeight = if (selected) FontWeight.Medium else FontWeight.Normal,modifier = modifier)
+//            if (imageRes != null){
+//                Image(painter = painterResource(id = imageRes), contentDescription = null,modifier = Modifier.padding(end = 8.dp))
+//            }
+//        }
+//    }
+}
